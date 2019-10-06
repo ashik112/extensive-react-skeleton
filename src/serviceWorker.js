58e75b33f75d693d19344b6a88c7a1bf9dbd4f57
Md. Ashekur Rahman <ashekur.rahman@eongroup.net.bd>
2019-10-06 09:50:03 +0600

    comment out console logs

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -42,10 +42,10 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
+          /* console.log(
             'This web app is being served cache-first by a service '
               + 'worker. To learn more, visit https://bit.ly/CRA-PWA',
-          );
+          ); */
         });
       } else {
         // Is not localhost. Just register service worker
@@ -72,10 +72,10 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
+              /* console.log(
                 'New content is available and will be used when all '
                   + 'tabs for this page are closed. See https://bit.ly/CRA-PWA.',
-              );
+              ); */
 
               // Execute callback
               if (config && config.onUpdate) {
@@ -85,9 +85,10 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+              // console.log('Content is cached for offline use.');
 
               // Execute callback
+              // eslint-disable-next-line no-lonely-if
               if (config && config.onSuccess) {
                 config.onSuccess(registration);
               }
@@ -96,8 +97,9 @@ function registerValidSW(swUrl, config) {
         };
       };
     })
+    // eslint-disable-next-line no-unused-vars
     .catch((error) => {
-      console.error('Error during service worker registration:', error);
+      // console.error('Error during service worker registration:', error);
     });
 }
 
@@ -123,9 +125,9 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      console.log(
+      /* console.log(
         'No internet connection found. App is running in offline mode.',
-      );
+      ); */
     });
 }
 
