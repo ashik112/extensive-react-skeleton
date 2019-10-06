ff8527d081e919e26babb8a477986d803d10b981
Md. Ashekur Rahman <ashekur.rahman@eongroup.net.bd>
2019-10-06 14:10:48 +0600

    working but needs optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
-import { Popconfirm, Icon } from 'antd';
+import Popconfirm from 'antd/es/popconfirm';
+import Icon from 'antd/es/icon';
 import logo from './logo.svg';
 import Badge from './components/Badge/Badge';
 import Button from './components/CustomButtons/Button';
 import './App.css';
+import LoginPage from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Badge color="primary">primary</Badge>
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Popconfirm
-          title="Are your sure?"
-          icon={<Icon type="question-circle" style={{ color: 'red' }} />}
-        >
-          <Button color="rose">
-            Test Antd
-          </Button>
-        </Popconfirm>
-      </header>
+      <LoginPage />
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <Badge color="primary">primary</Badge>*/}
+      {/*  <p>*/}
+      {/*    Edit*/}
+      {/*    <code>src/App.js</code>*/}
+      {/*    and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*  <Popconfirm*/}
+      {/*    title="Are your sure?"*/}
+      {/*    icon={<Icon type="question-circle" style={{ color: 'red' }} />}*/}
+      {/*  >*/}
+      {/*    <Button color="rose">*/}
+      {/*      Test Antd with material kit*/}
+      {/*    </Button>*/}
+      {/*  </Popconfirm>*/}
+      {/*</header>*/}
     </div>
   );
 }
