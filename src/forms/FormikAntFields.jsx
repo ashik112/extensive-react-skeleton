1cb5295ee7e2b9bfd005f8ccf975c810c6462c77
Md. Ashekur Rahman <ashekur.rahman@eongroup.net.bd>
2019-10-21 17:46:34 +0600

    Error state on Input, InputNumber & Select for Ant Design

--- src/forms/FormikAntFields.jsx
+++ src/forms/FormikAntFields.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types,react/jsx-props-no-spreading */
 import React from 'react';
 import { getIn } from 'formik';
-import {Input, InputNumber, Select} from 'antd';
+import {Input, InputNumber, Select, Tooltip} from 'antd';
 
 const { Option } = Select;
 
@@ -9,20 +9,28 @@ export const AntInput = ({ field, form, ...props }) => {
   // eslint-disable-next-line no-unused-vars
   const { values, errors, setFieldValue } = form;
   const errorMessage = getIn(errors, field.name);
+  const errorExist = errorMessage !== '' && errorMessage !== undefined;
   return (
     <>
-      <Input
-        size="small"
-        allowClear
-        {...props}
-        autoComplete="off"
-        /* eslint-disable-next-line react/jsx-props-no-spreading */
-        {...field}
-        onChange={(e) => {
-          setFieldValue(field.name, e.target.value);
-        }}
-      />
-      {errorMessage && <div style={{ color: 'red', fontSize: '0.7em' }}>{errorMessage}</div>}
+      <Tooltip
+        visible={errorExist}
+        destroyTooltipOnHide
+        title={errorExist && errorMessage}
+        placement="topRight"
+      >
+        <Input
+          className={(errorMessage && 'form-field-error') || ''}
+          allowClear
+          {...props}
+          autoComplete="off"
+          /* eslint-disable-next-line react/jsx-props-no-spreading */
+          {...field}
+          onChange={(e) => {
+            setFieldValue(field.name, e.target.value);
+          }}
+        />
+      </Tooltip>
+      {/*{errorMessage && <div style={{ color: 'red', fontSize: '0.7em' }}>{errorMessage}</div>}*/}
     </>
   );
 };
@@ -32,23 +40,31 @@ export const AntSelect = ({ field, form, ...props }) => {
   const { values, errors, setFieldValue } = form;
   const { options, selectedValue } = props;
   const errorMessage = getIn(errors, field.name);
+  const errorExist = errorMessage !== '' && errorMessage !== undefined;
   return (
     <>
-      <Select
-        size="small"
-        {...props}
-        allowClear
-        defaultValue={selectedValue || null}
-        style={{ width: '100%' }}
-        onChange={(value) => {
-          setFieldValue(field.name, value);
-        }}
+      <Tooltip
+        visible={errorExist}
+        destroyTooltipOnHide
+        title={errorExist && errorMessage}
+        placement="topRight"
       >
-        {options
-        && options.map((item) => <Option value={item.id} key={item.id}>{item.name}</Option>)}
-      </Select>
-      {errorMessage
-      && <div style={{ color: 'red', fontSize: '0.7em' }}>{errorMessage}</div>}
+        <Select
+          className={(errorMessage && 'form-field-error') || ''}
+          {...props}
+          allowClear
+          defaultValue={selectedValue || null}
+          style={{ width: '100%' }}
+          onChange={(value) => {
+            setFieldValue(field.name, value);
+          }}
+        >
+          {options
+          && options.map((item) => <Option value={item.id} key={item.id}>{item.name}</Option>)}
+        </Select>
+      </Tooltip>
+      {/*{errorMessage
+      && <div style={{ color: 'red', fontSize: '0.7em' }}>{errorMessage}</div>}*/}
     </>
   );
 };
@@ -57,20 +73,28 @@ export const AntInputNumber = ({ field, form, ...props }) => {
   // eslint-disable-next-line no-unused-vars
   const { values, errors, setFieldValue } = form;
   const errorMessage = getIn(errors, field.name);
+  const errorExist = errorMessage !== '' && errorMessage !== undefined;
   return (
     <>
-      <InputNumber
-        size="small"
-        style={{ width: '100%' }}
-        {...props}
-        autoComplete="off"
-        /* eslint-disable-next-line react/jsx-props-no-spreading */
-        {...field}
-        onChange={(e) => {
-          setFieldValue(field.name, e);
-        }}
-      />
-      {errorMessage && <div style={{ color: 'red', fontSize: '0.7em' }}>{errorMessage}</div>}
+      <Tooltip
+        visible={errorExist}
+        destroyTooltipOnHide
+        title={errorExist && errorMessage}
+        placement="topRight"
+      >
+        <InputNumber
+          className={(errorMessage && 'form-field-error') || ''}
+          style={{ width: '100%' }}
+          {...props}
+          autoComplete="off"
+          /* eslint-disable-next-line react/jsx-props-no-spreading */
+          {...field}
+          onChange={(e) => {
+            setFieldValue(field.name, e);
+          }}
+        />
+      </Tooltip>
+      {/*{errorMessage && <div style={{ color: 'red', fontSize: '0.7em' }}>{errorMessage}</div>}*/}
     </>
   );
 };
