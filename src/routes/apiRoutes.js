15561bfccfc1d2a1f077775972ccbdb867896cc0
Md. Ashekur Rahman <ashekur.rahman@eongroup.net.bd>
2019-12-09 15:17:42 +0600

    add centralized apiRoutes

---
+++ src/routes/apiRoutes.js
@@ -0,0 +1,22 @@
+import { apiUrl } from '../constants/config';
+
+function makeURL(baseURL, url) {
+  return `${apiUrl}${baseURL}${url}`;
+}
+
+export const baseURL = {
+  company: 'company/',
+  employee: 'employee/',
+};
+
+export const companyApiRoutes = {
+  list: makeURL(baseURL.company, 'list'),
+  company: (id) => makeURL(baseURL.company, `show/${id}`),
+  new: makeURL(baseURL.company, 'new'),
+  edit: (id) => makeURL(baseURL.company, `update/${id}`),
+  remove: (id) => makeURL(baseURL.company, `delete/${id}`),
+};
+
+export const employeeApiRoutes = {
+  datatable: makeURL('', 'employee_list_datatable'),
+};
