130f98d47928faca6547821a795673e895bfef43
Md. Ashekur Rahman <ashekur.rahman@eongroup.net.bd>
2019-12-10 08:57:02 +0600

    install libraries

---
+++ src/components/templates/Employee/EmployeeList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const $ = require('jquery');
+$.DataTable = require('datatables.net');
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class EmployeeList extends Component {
+  render() {
+    return (
+      <div>
+        f
+      </div>
+    );
+  }
+}
