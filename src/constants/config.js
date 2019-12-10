24a800e330b8f36c0ec61addea0d9683c3d2fc56
Md. Ashekur Rahman <ashekur.rahman@eongroup.net.bd>
2019-12-10 11:23:29 +0600

    change env var name

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BIZNET_API_URL_PROD : 'http://skeleton.local/app_dev.php/api/v1/';
+const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_SKELETON_API_URL_PROD : 'http://skeleton.local/app_dev.php/api/v1/';
 
 export {
   // eslint-disable-next-line import/prefer-default-export
